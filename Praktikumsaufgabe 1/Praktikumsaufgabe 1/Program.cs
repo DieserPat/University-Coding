fb5ea0142d185b4d7f17223ff74ea0db34c28fbf
DieserPat <103356329+DieserPat@users.noreply.github.com>
2022-07-01 15:43:18 +0200

    Add files via upload

---
+++ Praktikumsaufgabe 1/Praktikumsaufgabe 1/Program.cs
@@ -0,0 +1,195 @@
+﻿using System;
+
+namespace Praktikumsaufgabe_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CMySentence sentence = new CMySentence("Dies ist ein Test!");
+            CMyWord s = new CMyWord(sentence);
+            CMyWord t = new CMyWord(sentence[2] + " ");
+            Console.WriteLine(s);
+            Console.WriteLine(s + t);
+            Console.WriteLine(s - t);
+            Console.WriteLine(s[" :eraepsekahS"] + "S" + t | !("s" + t + "?"));
+            if (s) Console.WriteLine("s ist länger als 20 Zeichen.");
+            else Console.WriteLine("s ist höchstens 20 Zeichen lang.");
+            s *= 3;
+            Console.WriteLine(s);
+            if (s) Console.Write("s ist länger als 20 Zeichen ");
+            else Console.Write("s ist höchstens 20 Zeichen lang ");
+            Console.WriteLine("und enthält " + (int)sentence + " Wörter.");
+            if (new CMyWord("ein ") == t) Console.WriteLine("So soll es s" + t);
+        }
+        class CMyWord
+        {
+            private string zeichenkette;
+            public string Zeichenkette
+            {
+                get { return zeichenkette; }
+                set { zeichenkette = value; }
+            }
+            public CMyWord()
+            {
+                zeichenkette = "";
+            }
+            public CMyWord(String s)
+            {
+                zeichenkette = s;
+            }
+            // hier erweitern
+
+            public override string ToString()    //überladen des ToString() operanten
+            {
+                return Zeichenkette;
+            }
+            public CMyWord(CMySentence sentence)
+            {
+                zeichenkette = sentence.ToString();
+            }
+
+
+            // überladen der + operanten die objekte jeweils zusammenrechnen wollen mit verschiedenen parametern
+            // return jeweils mit der richtigen seite des strings eingefügt
+            public static CMyWord operator +(CMyWord satz1, CMyWord satz2)
+            {
+                return new CMyWord(satz1.Zeichenkette + satz2.Zeichenkette);
+            }
+            public static CMyWord operator +(string links, CMyWord satz2)
+            {
+                return new CMyWord(links + satz2.Zeichenkette);
+            }
+            public static CMyWord operator +(CMyWord satz2, string rechts)
+            {
+                return new CMyWord(satz2.Zeichenkette + rechts);
+            }
+
+
+
+            // "oder" operanten überladen sodass ein string eingefügt wird zwischen den zeichenketten
+            public static CMyWord operator |(CMyWord objekt1, CMyWord objekt2)
+            {
+                return new CMyWord(objekt1.Zeichenkette + "oder " + objekt2.Zeichenkette);
+            }
+
+
+            // 3x den string ausgeben
+            public static CMyWord operator *(CMyWord objekt, int zahl)
+            {
+                CMyWord text = new CMyWord();
+                for (int i = 0; i < zahl; i++)
+                {
+                    text += objekt + " ";
+                }
+                return text;
+            }
+
+            // operator der ein wort abzieht
+            public static CMyWord operator -(CMyWord satz1, CMyWord satz2)
+            {
+                return new CMyWord(satz1.Zeichenkette.Replace(satz2.Zeichenkette, ""));
+            }
+
+            //zeichenketten miteinander vergleichen
+            public static bool operator ==(CMyWord objekt1, CMyWord objekt2)
+            {
+                if (objekt1.Zeichenkette == objekt2.Zeichenkette)
+                    return true;
+                return false;
+            }
+
+            //vollständigkeitshalber muss der gegen operator angegeben werden
+            public static bool operator !=(CMyWord objekt1, CMyWord objekt2)
+            {
+                if (objekt1.Zeichenkette != objekt2.Zeichenkette)
+                    return true;
+                return false;
+            }
+
+
+            //True und False operator der die zeichenkette vergleicht mit der länge 20 (beide müssen dabei sein)
+            public static bool operator true(CMyWord w)
+            {
+                if (w.Zeichenkette.Length > 20)
+                {
+                    return true;
+                }
+                return false;
+            }
+            public static bool operator false(CMyWord w)
+            {
+                if (w.Zeichenkette.Length <= 20)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            // wie bei "oder" bloß als nicht operator wird das wort nicht eingefügt
+            public static CMyWord operator !(CMyWord objekt)
+            {
+                return new CMyWord("nicht " + objekt.Zeichenkette);
+            }
+
+            //indexer der den inhalt rückwärts ausgibt 
+            public CMyWord this[string text]
+            {
+                get
+                {
+                    string textrichtig = "";
+                    for (int i = text.Length - 1; i >= 0; i--)
+                    {
+                        textrichtig += text[i];
+                    }
+                    return new CMyWord(textrichtig);
+                }
+            }
+        }
+        class CMySentence
+        {
+            CMyWord[] myWords;
+            // hier erweitern
+
+            // sentence splitten und in CMyWord speichern
+            public CMySentence(string sentence)
+            {
+                string[] words = sentence.Split(' ');
+                myWords = new CMyWord[words.Length];
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    myWords[i] = new CMyWord(words[i]);
+                }
+            }
+
+            // expliziet operator zum zählen der wörter
+            public static explicit operator int(CMySentence objekt)
+            {
+                return objekt.myWords.Length;
+            }
+
+
+            //überladen ToString() der aus dem string array ein normalen string macht
+            public override string ToString()
+            {
+                string ausgabe = "";
+                for (int i = 0; i < myWords.Length; i++)
+                {
+                    ausgabe += myWords[i].Zeichenkette;
+                    ausgabe += " ";
+                }
+                return ausgabe;
+
+            }
+
+            //indexer zum konvertieren eines array indexinhalts in ein string
+            public string this[int i]
+            {
+                get { return myWords[i].ToString(); }
+            }
+
+            //ende danke :)
+        }
+    }
+}
---
+++ Praktikumsaufgabe_2/Praktikumsaufgabe_2/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Praktikumsaufgabe_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrioListe pl = new PrioListe();
+            pl.Einfuegen(" 9 Auto waschen", Prioritaet.niedrig);
+            pl.Einfuegen(" 6 Milch kaufen");
+            pl.Einfuegen(" 2 Brot nicht vergessen", Prioritaet.hoch);
+            pl.Einfuegen(" 3 Brief einwerfen", Prioritaet.hoch);
+            pl.Einfuegen(" 1 Praktikumsaufgabe hochladen", Prioritaet.top);
+            pl.Einfuegen(" 7 Praktikumsergebnisse anschauen");
+            pl.Einfuegen(" 4 Muttertagsgeschenk 9.5.", Prioritaet.hoch);
+            pl.Einfuegen("10 Weihnachtsgeschenke überlegen", Prioritaet.niedrig);
+            pl.Einfuegen(" 8 Listenstrukturen noch einmal durcharbeiten");
+            pl.Einfuegen(" 5 Steuererklärung bis 2.8.2021", Prioritaet.hoch);
+            pl.Ausgeben();
+        }
+        class PrioListe
+        {
+            public LElem head;
+
+            //Methode für mit den angegebenen Params
+            public void Einfuegen(string neueAufgabe, Prioritaet prio = Prioritaet.normal)
+            {
+                /* -direkt neues aufgaben objekt erstellen 
+                 * -hangler als pointer und durchgehen der liste
+                 * -vorgänger damit das neue objekt vom vorgänger verwiesen wird (als nachfolger)
+                 * -bool für die schleife
+                 */
+                LElem neu = new LElem(neueAufgabe, prio);
+                LElem hangler = head;
+                LElem vorgaenger;
+                bool gefunden = false;
+
+                //wenn es noch keinen head gibt
+                if (head == null)
+                {
+                    head = neu;
+                }
+                //wenn es nur EIN element gibt und das einzige Element NIEDRIGERE prio hat als das neue --> hinten anfügen
+                else if (head.nachfolger == null && head.prioAufgabe < neu.prioAufgabe)
+                {
+                    head.nachfolger = neu;
+                }
+                //wenn es nur EIN element gibt und es HÖHERE prio hat als das NEUE --> vorne anfügen
+                else if (head.nachfolger == null && head.prioAufgabe > neu.prioAufgabe)
+                {
+                    neu.nachfolger = head;
+                    head = neu;
+                }
+                //wenn die prio der neuen aufgabe "top" ist oder die neue priorität schon HÖHER als die prio des HEADS --> vorne 
+                else if (neu.prioAufgabe == Prioritaet.top || head.prioAufgabe > neu.prioAufgabe)
+                {
+                    head = neu;
+                    neu.nachfolger = hangler;
+                }
+
+                //schleife die alle aufgaben durchgeht und sie and der stelle einfügt an der sie vorgesehen bis bool = false
+                else
+                {
+                    while (!gefunden)
+                    {
+                        vorgaenger = hangler;
+                        hangler = hangler.nachfolger;
+
+                        if ((int)hangler.prioAufgabe > (int)neu.prioAufgabe)
+                        {
+                            vorgaenger.nachfolger = neu;
+                            neu.nachfolger = hangler;
+                            gefunden = true;
+                        }
+                        else if (hangler.nachfolger == null)
+                        {
+                            hangler.nachfolger = neu;
+                            gefunden = true;
+                        }
+                    }
+                }
+            }
+
+            //ausgabe der anstehenden Aufgabe und löschen(abgearbeitet) der Aufgabe
+            public void NaechsteAufgabe()
+            {
+                if (head != null)
+                {
+                    Console.WriteLine($"Die nächste Aufgabe ist: {head.aufgabe}");
+                    head = head.nachfolger;
+                }
+            }
+
+            //ausgabe aller aufgaben auf die console
+            public void Ausgeben()
+            {
+                LElem hangler = head;
+                while (hangler != null)
+                {
+                    Console.WriteLine($"{hangler.prioAufgabe,8}: {hangler.aufgabe}");
+                    hangler = hangler.nachfolger;
+                }
+            }
+
+            //class Listenelement
+            public class LElem
+            {
+                //objectverweis "nachfolger"
+                public string aufgabe;
+                public LElem nachfolger;
+                public Prioritaet prioAufgabe;
+
+                //normaler Konstruktor für Listenelement
+                public LElem(string aufgabe, Prioritaet prio)
+                {
+                    this.aufgabe = aufgabe;
+                    nachfolger = null;
+                    prioAufgabe = prio;
+                }
+            }
+        }
+
+       
+
+        // enum priorität. Hier arbeite ich mit den index wobei top für "0" steht und niedrig für "3"
+        enum Prioritaet { top, hoch, normal, niedrig }
+    }
+
+}
